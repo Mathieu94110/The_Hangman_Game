e1e9d233c2a1ac9b0efb6b8343d7e0f08eb9eb7e
Mathieu Enault <me94110@gmail.com>
2020-11-22 15:45:00 +0100

    Updates

---
+++ precache-manifest.299104ebd4e5b930fc9b25a1e4be2ddd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "498d3a4b83e9d285e79503359f380823",
+    "url": "/The_Hangman_Game/index.html"
+  },
+  {
+    "revision": "ed916ecf94b762472762",
+    "url": "/The_Hangman_Game/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ec5b5c09e77334c39109",
+    "url": "/The_Hangman_Game/static/css/main.42fb6155.chunk.css"
+  },
+  {
+    "revision": "ed916ecf94b762472762",
+    "url": "/The_Hangman_Game/static/js/2.cb39749e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/The_Hangman_Game/static/js/2.cb39749e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec5b5c09e77334c39109",
+    "url": "/The_Hangman_Game/static/js/main.b2b4e67e.chunk.js"
+  },
+  {
+    "revision": "0156effccd921c3dc74f",
+    "url": "/The_Hangman_Game/static/js/runtime-main.b868226e.js"
+  }
+]);
